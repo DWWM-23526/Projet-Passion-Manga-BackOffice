39402ebec45bb7dc7d83d7845818438ca77309aa
Pierre-Marie <pierremariemarchio@gmail.com>
2024-08-22 15:05:45 +0200

    [dataprovider] added getAll get one and update

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,5 +1,37 @@
-import simpleRestProvider from "ra-data-simple-rest";
+import { DataProvider, fetchUtils } from "react-admin";
 
-export const dataProvider = simpleRestProvider(
-  import.meta.env.VITE_SIMPLE_REST_URL,
-);
+const API_URL = import.meta.env.VITE_SIMPLE_REST_URL;
+
+const dataProvider: DataProvider = {
+  getList: async (resource, params) => {
+    const response = await fetchUtils.fetchJson(`${API_URL}/${resource}`);
+
+    return {
+      data: response.json.data,
+      total: parseInt(response.headers.get("x-total-count") || "", 10),
+    };
+  },
+  getOne: async (resource, params) => {
+    const response = await fetchUtils.fetchJson(
+      `${API_URL}/${resource}/${params.id}`,
+    );
+
+    return {
+      data: response.json.data,
+    };
+  },
+  update: async (resource, params) => {
+    const {id, ...data} = params.data;
+    const response = await fetchUtils.fetchJson(
+        `${API_URL}/${resource}/${id}`,
+        {
+            method: "PUT",
+            body: JSON.stringify(data)
+        }
+    );
+
+    return {data: response.json.data}
+  },
+};
+
+export { dataProvider };
