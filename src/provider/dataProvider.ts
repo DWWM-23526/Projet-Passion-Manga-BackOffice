1f1de912cb52068424c0bd424318acba19d2bdf7
Pierre-Marie <pierremariemarchio@gmail.com>
2024-08-22 16:48:57 +0200

    [hot fix] Authorization to authorization

--- src/provider/dataProvider.ts
+++ src/provider/dataProvider.ts
@@ -1,11 +1,13 @@
+
 import { DataProvider, fetchUtils } from "react-admin";
 
 const API_URL = import.meta.env.VITE_SIMPLE_REST_URL;
+const token = localStorage.getItem("authToken");
 
 const dataProvider: DataProvider = {
   getList: async (resource, params) => {
     const response = await fetchUtils.fetchJson(`${API_URL}/${resource}`);
-
+    console.log(token);
     return {
       data: response.json.data,
       total: parseInt(response.headers.get("x-total-count") || "", 10),
@@ -22,14 +24,20 @@ const dataProvider: DataProvider = {
   },
   update: async (resource, params) => {
     const {id, ...data} = params.data;
+    console.log(token);
     const response = await fetchUtils.fetchJson(
         `${API_URL}/${resource}/${id}`,
         {
             method: "PUT",
-            body: JSON.stringify(data)
+            body: JSON.stringify(data),
+            headers: new Headers({
+              'Content-Type': 'application/json',
+              'authorization': `Bearer ${token}`, 
+          }),
         }
     );
 
+    
     return {data: response.json.data}
   },
 };
